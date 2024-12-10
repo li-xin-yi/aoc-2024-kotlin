180c84ee4d58ca3265bc7378a3f7b765d2faca11
Xinyi Li <wolixinyi@gmail.com>
2024-12-10 01:28:29 -0800

    day 8 part2

--- python/day08-2.py
+++ python/day08-2.py
@@ -4,18 +4,26 @@ with open('../inputs/day08.txt') as f:
     lines = f.readlines()
 
 antennas = defaultdict(list)
+res = set()
 n, m = len(lines), len(lines[0].strip())
 for i in range(n):
     for j in range(m):
         if lines[i][j] != '.':
             antennas[lines[i][j]].append((i, j))
+            res.add((i, j))
 
 def get_antinodes(x1, y1, x2, y2):
     dx = x2 - x1
     dy = y2 - y1
-    return (x2 + dx, y2 + dy), (x1 - dx, y1 - dy)
+    i = 1
+    while 0 <= x2 + i * dx < n and 0 <= y2 + i * dy < m:
+        yield x2 + i * dx, y2 + i * dy
+        i += 1
+    i = 1
+    while 0 <= x1 - i * dx < n and 0 <= y1 - i * dy < m:
+        yield x1 - i * dx, y1 - i * dy
+        i += 1
 
-res = set()
 
 
 for freq in antennas:
@@ -24,7 +32,6 @@ for freq in antennas:
         for j in range(i + 1, len(lst)):
             a, b = lst[i], lst[j]
             for x, y in get_antinodes(*a, *b):
-                if 0 <= x < n and 0 <= y < m and (x, y) not in res:
+                if (x, y) not in res:
                     res.add((x, y))
-
 print(len(res))
