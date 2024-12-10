ed7092a5abcce4b78b5ab3dcb1d29c39a9a28d7a
Xinyi Li <wolixinyi@gmail.com>
2024-12-10 02:23:34 -0800

    day07

---
+++ python/day07-2.py
@@ -0,0 +1,30 @@
+from collections import defaultdict
+
+with open('../inputs/day07.txt') as f:
+    lines = f.readlines()
+
+def check_feasible(result, operands):
+    cur = set([operands[0]])
+    for num in operands[1:]:
+        temp = set()
+        for prev in cur:
+            if prev + num <= result:
+                temp.add(prev + num)
+            if prev * num <= result:
+                temp.add(prev * num)
+            if int(str(prev) + str(num)) <= result:
+                temp.add(int(str(prev) + str(num)))
+        if not temp:
+            return False
+        cur = temp
+    return result in cur
+
+res = 0
+for line in lines:
+    left, right = line.strip().split(':')
+    operands = list(map(int, right.strip().split(' ')))
+    left = int(left)
+    if check_feasible(left, operands):
+        res += left
+
+print(res)
